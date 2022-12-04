fa678a35c23e257b52b9e54a1050d48addf676c5
nirjagothi <nirja.gothi@codezeros.com>
2022-12-04 10:49:00 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [dataList, setDataList] = useState([]);
+  const [selectedData, setSelectedData] = useState({})
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json()).then(
+      data => setDataList(data)
+    )
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='firstDiv'></div>
+      <div className='flexItem'>
+        <div className='width40 leftDiv'>
+          {dataList.map((user, i) => (
+            <div key={i} className="singleBox" onClick={() => setSelectedData(user)}>
+              <div>{user.name}</div>
+              <div>{user.email}</div>
+            </div>
+          ))}
+        </div>
+        <div className='width60 padding20'>
+          <div>
+            <div>Name: &nbsp;&nbsp;&nbsp;{selectedData.name}</div>
+            <div>Email: &nbsp;&nbsp;&nbsp;{selectedData.email}</div>
+            <div>City: &nbsp;&nbsp;&nbsp;{selectedData?.address?.city}</div>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
